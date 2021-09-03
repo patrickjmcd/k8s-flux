c35558348f149ceed5a1fd5ebd97e6d45505ddc8
Patrick McDonagh <patrick@meshify.com>
2021-09-03 11:35:48 -0500

    pushes settings after updating the file

--- setup/95-update-cluster-settings.sh
+++ setup/95-update-cluster-settings.sh
@@ -4,3 +4,4 @@ source env.vars
 
 rm -rf ../cluster/base/cluster-settings.yaml
 envsubst < ../tmpl/cluster-settings.yaml > ../cluster/base/cluster-settings.yaml
+kubectl apply -f ../cluster/base/cluster-settings.yaml
